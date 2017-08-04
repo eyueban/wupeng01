8341568e350cebd707c5bdb9d434cc471d6058e2
viphone <18516966845@163.com>
2017-08-04 19:41:20 +0800

    Initial commit

---
+++ src/main/java/NewHelloWord.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by Mr.W on 2017/8/4.
+ */
+public class NewHelloWord {
+    @Test
+    public void sayHelloWrod() {
+        System.out.println("hello word");
+    }
+}
